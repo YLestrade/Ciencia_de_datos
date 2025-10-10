46f87ea887c39ba4204fa101d286e53fa634c181
YLestrade <qcyendi@gmail.com>
2025-10-09 19:29:25 -0600

    Mi primer script
    En este script se encuentran mis primeros comandos.

---
+++ mi primer script.R
@@ -0,0 +1,23 @@
+# Hola
+# aquí va el código
+# en la ventana de abajo, va a salir el resultado 
+# de nuestro análisis
+
+
+# cargar matriz de datos iris en
+# el objeto BD que significa
+# Base de Datos
+BD<-iris
+
+# este comando sirve para visualizar
+# una matriz de datos
+View(BD)
+
+#str visualiza y arroja el tipo de variable
+# hay dos tipo de varibles son cuantitativas y cualitativas
+str(iris)
+str(BD)
+
+
+
+
